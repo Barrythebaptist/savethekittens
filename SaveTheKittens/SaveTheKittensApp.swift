6b12d97da63fc248a5dde9950ca44f22e7af2b5e
jeff <barrythebaptist@github.com>
2020-10-21 12:25:08 -0600

    Initial Commit

---
+++ SaveTheKittens/SaveTheKittensApp.swift
@@ -0,0 +1,17 @@
+//
+//  SaveTheKittensApp.swift
+//  SaveTheKittens
+//
+//  Created by Jeffery Mahood on 2020-10-21.
+//
+
+import SwiftUI
+
+@main
+struct SaveTheKittensApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
